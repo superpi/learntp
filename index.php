a334030065a09987d17a1a383093727ca612b659
Pi <ixdupi@gmail.com>
2014-11-09 17:44:49 +0800

    初始化入口文件

--- index.php
+++ index.php
@@ -18,9 +18,6 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define('APP_DEBUG',True);
 
 //生成目录区域
-define('BIND_MODULE','Auth');
-define('BUILD_CONTROLLER_LIST','Index');
-define('BUILD_MODEL_LIST','User');
 
 // 定义应用目录
 define('APP_PATH','./Application/');
